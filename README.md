a3c164ff2fa03c5517ecae8d0cee8cc92d6f96fe
Christopher Bradford <christopher.bradford@onlive.com>
2011-12-12 00:52:52 -0800

    Successfully connecting and negotiating TLS. Added more text to the project readme and notes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 BOT
 ===
+BOT is an extendable XMPP bot. This tool sits in group chats on a Jabber server 
+listening to conversations happening around/to it. Upon seeing a message it 
+fires a call into registered plugins looking for a match to the message. Should
+one be found its method is called and the appropriate action is required.
+
+Plugins may register themselves quite easily and a simple API is provided for
+sending messages to the monitored room.
+
+Where Am I?
+===========
+Currently BOT is starting up the XMPP streams and negotiating through the 
+STARTTLS sequence. Next up is authentication.
+
--- bot.js
+++ bot.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 
 var username = process.argv[2];
-var domain = process.argv[2];
+var domain = process.argv[3];
 var jabber_server = 'talk.google.com';
+
+if (username && domain) {
+  console.log(username + '@' + domain);
+}
+else {
+  exit
+}
 
 // Require the net module
 var net = require('net');
 
 // Require TLS/SSL
 var tls = require('tls');
+
+// TLS Helpers
+var starttls = require('./starttls.js');
 
 // Require the xml parsing library and create a parser
 var xml2js = require('xml2js');
@@ -17,56 +27,106 @@ var parser = new xml2js.Parser({explicitRoot: true});
 // Require the stream factory
 var stream_factory = require('./stream_factory.js');
 
-// Create a placeholder for out secure_client
-var secure_client = null;
+// Fire up the client
+var old_client = null;
+var client = net.connect(5222, jabber_server);
+register_insecure_client_handlers();
 
-// Fire up the insecure_client
-var insecure_client = net.connect(5222, jabber_server);
-
-// When our socket connects we start the stream
-insecure_client.on('connect', function () {
-  console.log('Insecure Client connected');
-  
-  var start_stream = stream_factory.start_stream(username, domain);
-  console.log('Starting Insecure Stream', start_stream);
-  
-  insecure_client.write(start_stream);
-});
-
-// Global handler of messages from the insecure socket
-insecure_client.on('data', function (data) {
-  console.log ('Insecure Received Data!');
-  var response = data.toString('utf8');
-  console.log(response);
-  
-  // Check to see if this is the start of a stream
-  if (response.substr(0, 14) == '<stream:stream') {
-    console.log("Start stream, automatically close element");
+// Register insecure handlers
+function register_insecure_client_handlers() {
+  // When our socket connects we start the stream
+  client.on('connect', function () {
+    console.log('Insecure Client Connected');
     
-    // Automatically close the element as to not confuse our parser
-    response = response + "</stream:stream>";
-  }
-  
-  // Parse the XML payload
-  parser.parseString(response, function (err, payload) {
-    if (err) {
-      console.log("ERROR PARSING XML: ", response);
-      return;
-    }
+    var start_stream = stream_factory.start_stream(username, domain);
+    console.log('Starting Insecure Stream', start_stream);
     
-    console.log(payload);
-    
-    // Handle various states, in the future have hooks where classes register for messages they are interested in.
-    if (payload) {
-      handle_payload(payload);
-    }
-    else {
-      // This was a null data response :(
-    }
+    client.write(start_stream);
   });
-  
-  console.log("========================================================")
-});
+
+  // Global handler of messages from the insecure socket
+  client.on('data', function (data) {
+    console.log ('Insecure Received Data!');
+    var response = data.toString('utf8');
+    console.log(response);
+    
+    // Check to see if this is the start of a stream
+    if (response.substr(0, 14) == '<stream:stream') {
+      console.log("Start stream, automatically close element");
+      
+      // Automatically close the element as to not confuse our parser
+      response = response + "</stream:stream>";
+    }
+    
+    // Parse the XML payload
+    parser.parseString(response, function (err, payload) {
+      if (err) {
+        console.log("ERROR PARSING XML: ", response);
+        return;
+      }
+      
+      console.log(payload);
+      
+      // Handle various states, in the future have hooks where classes register for messages they are interested in.
+      if (payload) {
+        handle_payload(payload);
+      }
+      else {
+        // This was a null data response :(
+      }
+    });
+    
+    console.log("========================================================");
+  });
+}
+
+// Register secure handlers
+function register_secure_client_handlers() {
+  // When our socket connects we start the stream
+  client.on('connect', function () {
+    console.log('Secure Client Connected');
+    
+    var start_stream = stream_factory.start_stream(username, domain);
+    console.log('Starting Secure Stream', start_stream);
+    
+    client.write(start_stream);
+  });
+
+  // Global handler of messages from the insecure socket
+  client.on('data', function (data) {
+    console.log ('Secure Received Data!');
+    var response = data.toString('utf8');
+    console.log(response);
+    
+    // Check to see if this is the start of a stream
+    if (response.substr(0, 14) == '<stream:stream') {
+      console.log("Start stream, automatically close element");
+      
+      // Automatically close the element as to not confuse our parser
+      response = response + "</stream:stream>";
+    }
+    
+    // Parse the XML payload
+    parser.parseString(response, function (err, payload) {
+      if (err) {
+        console.log("ERROR PARSING XML: ", response);
+        return;
+      }
+      
+      console.log(payload);
+      
+      // Handle various states, in the future have hooks where classes register for messages they are interested in.
+      if (payload) {
+        handle_payload(payload);
+      }
+      else {
+        // This was a null data response :(
+      }
+    });
+    
+    console.log("========================================================");
+  });
+}
 
 // Generic handler
 function handle_payload(payload) {
@@ -97,11 +157,24 @@ function handle_features(features_payload) {
     // We are currently not sending over a secure channel, start that process
     var start_tls = stream_factory.start_tls();
     console.log('Starting TLS', start_tls);
-    insecure_client.write(start_tls);
+    client.write(start_tls);
   }
 }
 
 // Handles proceed responses (used to start TLS)
 function handle_proceed(proceed_payload) {
   console.log('Received Proceed, starting tls connection');
+  
+  // Unregister old handlers
+  old_client = client;
+  old_client.removeAllListeners();
+  
+  // Starttls
+  client = starttls(client, {}, function () {
+    console.log("In Callback, Emitting connect call");
+    client.emit('connect');
+  });
+  
+  // Register new handlers
+  register_secure_client_handlers();
 }
